4b828b2b9aaaa6659d4fab9f6da39bfa12a521b9
success ibekwe <Successibekwe047@gmail.com>
2024-10-20 03:49:23 +0100

    added head meta

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+useHead({
+  title: "My Dating App",
+  meta: [{ name: "description", content: "My amazing site." }],
+  bodyAttrs: {
+    class: "test",
+  },
+  script: [{ innerHTML: "console.log('Hello world')" }],
+});
+</script>
+
 <template>
   <div>
     <HeroSection />
